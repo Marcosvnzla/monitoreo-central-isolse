5c4dd29218447980e382f7fe30f8e7891689a3bd
Marcos Ulloa <marcosdavidulloa@gmail.com>
2021-01-24 19:41:18 -0300

    Added form change listener

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,11 +2,26 @@ import React, { Component, Fragment } from 'react';
 import Input from '../../components/UI/Input/Input';
 
 class Auth extends Component {
+  state = {
+    formData: {
+      email: {
+        value: ''
+      },
+      password: {
+        value: ''
+      },
+    }
+  }
+
+  inputChanged (event) {
+    console.log(event.target.value);
+  }
+
   render () {
     let form = (
       <form>
-        <Input elementType="input" label="email"></Input> 
-        <Input elementType="input" label="password"></Input> 
+        <Input elementType="input" label="email" value={this.state.formData.email.value} changed={this.inputChanged} /> 
+        <Input elementType="input" label="password" value={this.state.formData.password.value} changed={this.inputChanged} />
       </form>
     );
 
